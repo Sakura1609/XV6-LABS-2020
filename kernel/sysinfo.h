2e8efe994bc1fae746a705843e6fe38f7a339849
Sakura1609 <3078688722@qq.com>
2022-10-02 09:55:54 +0800

    修改了sysinfo.h中错误的注释

--- kernel/sysinfo.h
+++ kernel/sysinfo.h
@@ -1,5 +1,4 @@
 struct sysinfo {
-  // struct sleeplock lock; // protects everything below here
   uint64 freemem;   // amount of free memory (bytes)
   uint64 nproc;     // number of process
   uint64 freefd;    // number of free file descriptor
