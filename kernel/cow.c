71aa28d926b7c70e371575c134484bdbe1db9467
root <root@LAPTOP-QTP8C8V6.localdomain>
2022-11-02 23:12:27 +0800

    lab6: cow-decrease func with some problem

---
+++ kernel/cow.c
@@ -0,0 +1,49 @@
+#include "types.h"
+#include "riscv.h"
+#include "param.h"
+#include "memlayout.h"
+#include "defs.h"
+#include "spinlock.h"
+struct
+{
+    struct spinlock lock;
+    uint overpages;
+} imple[(PHYSTOP - KERNBASE) >> 12];
+
+void increase(uint64 pa)
+{
+    if (pa < KERNBASE)
+        return;
+    int i = (pa - KERNBASE) >> 12;
+    acquire(&imple[i].lock);
+    imple[i].overpages++;
+    release(&imple[i].lock);
+}
+
+uint decrease(uint64 pa)
+{
+    uint ret;
+    if (pa < KERNBASE)
+    {
+        return 0;
+    }
+    pa = (pa - KERNBASE) >> 12;
+    acquire(&imple[pa].lock);
+    ret = --imple[pa].overpages;
+    release(&imple[pa].lock);
+    return ret;
+}
+
+// uint decrease(uint64 pa)
+// {
+//     uint ret;
+//     if (pa < KERNBASE)
+//     {
+//         return 0;
+//     }
+//     pa = (pa - KERNBASE) >> 12;
+//     acquire(&imple[pa].lock);
+//     --imple[pa].overpages;
+//     release(&imple[pa].lock);
+//     return imple[pa].overpages;
+// }
--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -14,53 +14,65 @@ void freerange(void *pa_start, void *pa_end);
 extern char end[]; // first address after kernel.
                    // defined by kernel.ld.
 
-struct run {
+struct run
+{
   struct run *next;
 };
 
-struct {
+struct
+{
   struct spinlock lock;
   struct run *freelist;
 } kmem;
 
-void
-kinit()
+void kinit()
 {
   initlock(&kmem.lock, "kmem");
-  freerange(end, (void*)PHYSTOP);
+  freerange(end, (void *)PHYSTOP);
 }
 
-void
-freerange(void *pa_start, void *pa_end)
+void freerange(void *pa_start, void *pa_end)
 {
   char *p;
-  p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  p = (char *)PGROUNDUP((uint64)pa_start);
+  // printf("%p %p\n", KERNBASE, end);
+  for (; p + PGSIZE <= (char *)pa_end; p += PGSIZE)
+  {
+    increase((uint64)p);
     kfree(p);
+  }
 }
 
 // Free the page of physical memory pointed at by v,
 // which normally should have been returned by a
 // call to kalloc().  (The exception is when
 // initializing the allocator; see kinit above.)
-void
-kfree(void *pa)
+void kfree(void *pa)
 {
   struct run *r;
 
-  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+  if (((uint64)pa % PGSIZE) != 0 || (char *)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
+  // decrease implements
+  if (decrease((uint64)pa))
+  {
+    return;
+  }
+
+  // if implement is none then
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
 
-  r = (struct run*)pa;
+  r = (struct run *)pa;
 
   acquire(&kmem.lock);
   r->next = kmem.freelist;
   kmem.freelist = r;
   release(&kmem.lock);
 }
+
+
 
 // Allocate one 4096-byte page of physical memory.
 // Returns a pointer that the kernel can use.
@@ -72,11 +84,13 @@ kalloc(void)
 
   acquire(&kmem.lock);
   r = kmem.freelist;
-  if(r)
+  if (r)
     kmem.freelist = r->next;
   release(&kmem.lock);
 
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
-  return (void*)r;
+  increase((uint64)r);
+
+  if (r)
+    memset((char *)r, 5, PGSIZE); // fill with junk
+  return (void *)r;
 }
